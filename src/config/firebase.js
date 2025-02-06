3ce7184ea5e57bd20fdbf519149a6c0fb382e295
fleeted <pikachu12314@gmail.com>
2025-02-06 00:17:12 -0500

    added the firestore database connection

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"; // Add this line
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyCOpVLxfmJ5wG4BPHj_JWjlGZj4dOfkum8",
@@ -15,5 +17,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app); // Initialize Firestore
 
-export { auth };
+export { auth, db};
