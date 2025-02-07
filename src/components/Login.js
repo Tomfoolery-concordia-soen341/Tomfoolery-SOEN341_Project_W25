45079c97ccec101eae68df21c750a084fd7b7a52
Gilbert Sedillo <gtsedillo@me.com>
2025-02-07 09:39:50 -0500

    place checkAdmin into Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,50 @@ import React, { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../config/firebase";
 import { useNavigate } from "react-router-dom";
+import { database } from "../config/firebase";
+import { getDoc, setDoc, doc, collection } from "firebase/firestore";
+
+export let isAdmin = false;
+
+//this function takes a user object in
+export async function checkAdmin(user) {
+
+  //finds the user in the firestore
+  const userRef = await doc(database, "users", user.uid);
+
+  //snapshot of the specific user
+  const specificUser = await getDoc(userRef);
+
+  //this var allows us to pick the field in the specific user
+  const field = specificUser.data();
+
+  console.log(field.admin);
+
+  //check if the user exists
+  if (specificUser.exists()) {
+
+    //check if the user is an admin and return result
+    if (field.admin === true) {
+      return true;
+    } else {
+      return false;
+    }
+
+    //if the user does not exist in firestore create one using the user object passed in
+  } else {
+
+    //get the location of where the user documents data are stored
+    const userRefForNew = collection(database, "users");
+
+    //create a new doc with the uid as the name and set the admin to false
+    await setDoc(doc(userRefForNew, user.uid), {
+      email: user.email,
+      admin: false
+    });
+
+    return false;
+  }
+}
 
 const Login = () => {
   const [email, setEmail] = useState("");
