36bfce8d09bd4df0a1ec448b6ecb1e32bbc9fd6d
Gilbert Sedillo <gtsedillo@me.com>
2025-02-07 00:15:07 -0500

    import firestore

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyCOpVLxfmJ5wG4BPHj_JWjlGZj4dOfkum8",
