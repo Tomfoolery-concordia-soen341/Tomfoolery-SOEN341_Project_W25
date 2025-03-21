f534a6bdd1949701ea7a62a447efd3b334156bec
Gilbert Sedillo <gtsedillo@me.com>
2025-03-20 20:55:24 -0400

    update channels when dialog shown/hid

--- src/components/MemberDash.js
+++ src/components/MemberDash.js
@@ -18,7 +18,7 @@ const MemberDash = () => {
 
   useEffect(() => {
     fetchChannels();
-  }, [user]); //it only gets triggered when user changes
+  }, [user, dialogShow]); //it only gets triggered when user changes
 
   const GoToFriendsList = () => {
     navigate("/Mfriends"); // Redirect to the friends list page
